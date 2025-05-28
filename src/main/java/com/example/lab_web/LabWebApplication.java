be13713f8d0be90ef3bcd8620b54b4d597d77241
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-05-28 09:04:01 -0300

    primeiro commit

---
+++ src/main/java/com/example/lab_web/LabWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabWebApplication.class, args);
+	}
+
+}
