dab1b1848103c1230719865992637c45fd58975e
Alana Abreu <149194023+N1nh4@users.noreply.github.com>
2025-06-23 12:30:56 -0300

    tirando id já que é herança

--- src/main/java/com/example/lab_web/Model/Administrador.java
+++ src/main/java/com/example/lab_web/Model/Administrador.java
@@ -3,19 +3,11 @@ package com.example.lab_web.Model;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 
 @Entity
 public class Administrador extends Usuario {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
     private String cpf;
     private String endereco;
     private String telefone;
