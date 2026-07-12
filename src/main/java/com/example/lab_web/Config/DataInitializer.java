9b05506f9b952450268490c8edd48c920f13152a
Alana <alanaabreu1103@gmail.com>
2026-07-11 22:30:01 -0300

    o

--- src/main/java/com/example/lab_web/Config/DataInitializer.java
+++ src/main/java/com/example/lab_web/Config/DataInitializer.java
@@ -1,28 +1,13 @@
 package com.example.lab_web.Config;
 
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.jdbc.datasource.init.ScriptUtils;
 import org.springframework.stereotype.Component;
-
-import javax.sql.DataSource;
-import java.sql.Connection;
 
 @Component
 public class DataInitializer implements CommandLineRunner {
 
-    private final DataSource dataSource;
-
-    public DataInitializer(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
-
     @Override
     public void run(String... args) throws Exception {
-        try (Connection conn = dataSource.getConnection()) {
-            System.out.println("[DataInitializer] Limpando e reimportando dados...");
-            ScriptUtils.executeSqlScript(conn, new ClassPathResource("import.sql"));
-            System.out.println("[DataInitializer] Dados importados com sucesso.");
-        }
+        System.out.println("[DataInitializer] Desabilitado - usando ddl-auto=create");
     }
 }
